c4598a3ccb4ac3f60ebc5ba418280af1306d2157
pozniako16 <lev.pozniakoff@gmail.com>
2021-04-18 17:17:08 +0200

    added view of calendar in fxml file

---
+++ src/main/java/ch/heigvd/pro/calendarview/CalendarView.java
@@ -0,0 +1,16 @@
+package ch.heigvd.pro.calendarview;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CalendarView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
